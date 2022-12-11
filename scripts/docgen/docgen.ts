34093b9521bb4281119d4cf30663c7d5dbad4f4e
soheil-saberi <saberi.soheil74@gmail.com>
2022-12-11 13:32:23 +0330

    FIX

--- scripts/docgen/docgen.ts
+++ scripts/docgen/docgen.ts
@@ -5,16 +5,16 @@ import { generateDeclarations } from './generate-declarations';
 
 // Paths for components that require props information extraction
 const PATHS = [
-  '../../src/components/Calendar/Calendar.tsx',
-  '../../src/components/DateInput/DateInput.tsx',
-  '../../src/components/DatePicker/DatePicker.tsx',
-  '../../src/components/DatePickerInput/DatePickerInput.tsx',
-  '../../src/components/DateTimePicker/DateTimePicker.tsx',
-  '../../src/components/MonthPicker/MonthPicker.tsx',
-  '../../src/components/MonthPickerInput/MonthPickerInput.tsx',
-  '../../src/components/TimeInput/TimeInput.tsx',
-  '../../src/components/YearPicker/YearPicker.tsx',
-  '../../src/components/YearPickerInput/YearPickerInput.tsx',
+  '../../src/components/JalaliCalendar/JalaliCalendar.tsx',
+  '../../src/components/JalaliDateInput/JalaliDateInput.tsx',
+  '../../src/components/JalaliDatePicker/JalaliDatePicker.tsx',
+  '../../src/components/JalaliDatePickerInput/JalaliDatePickerInput.tsx',
+  '../../src/components/JalaliDateTimePicker/JalaliDateTimePicker.tsx',
+  '../../src/components/JalaliMonthPicker/JalaliMonthPicker.tsx',
+  '../../src/components/JalaliMonthPickerInput/JalaliMonthPickerInput.tsx',
+  '../../src/components/JalaliTimeInput/JalaliTimeInput.tsx',
+  '../../src/components/JalaliYearPicker/JalaliYearPicker.tsx',
+  '../../src/components/JalaliYearPickerInput/JalaliYearPickerInput.tsx',
 ].map((filePath) => path.join(dirname(fileURLToPath(import.meta.url)), filePath));
 
 fs.writeJSONSync(
